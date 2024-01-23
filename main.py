39f3619e82dc69c350f048731251bad15e1ed261
Hamza Khan <hamzasafdark@gmail.com>
2024-01-23 17:15:18 +0500

    generate report of listed books

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    generate_report(count_letters(text))
+    word_count = count_words(text)
+    letters_dict = count_letters(text)
+    letters_sorted_list = letters_dict_to_sorted_list(letters_dict)
+    
+    print('--- Begin report of %s ---\n %i words froun in the document\n\n' % (book_path, word_count))
+
+    for item in letters_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print('--- End report ---')
 
 
 
@@ -14,21 +25,27 @@ def count_words(text):
     return len(words)
 
 def count_letters(text):
-    letters = text.lower()
     letter_mapping = {}
-    count = 0
 
-    for c in [*letters]:
-        try:
-            letter_mapping[c] += 1
-        except:
-            letter_mapping[c] = 1
-
-def generate_report(data):
-    
+    for c in text:
+        lowerc = c.lower()
+        if lowerc in letter_mapping:
+             letter_mapping[lowerc] += 1
+        else:
+            letter_mapping[lowerc] = 1
 
     return letter_mapping
 
-    
+def sort_on(d):
+    return d["num"]
+
+
+def letters_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 main()
 
